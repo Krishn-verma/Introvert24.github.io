869736b853f88594a903215a9adc3c96b84cdb2e
Introvert <krishv594@gmail.com>
2023-11-10 15:58:23 +0530

    rev

--- src/Html/index.jsx
+++
@@ -1,6 +0,0 @@
-import Guide from "./Guide";
-import Subheaders from "./Subheaders";
-import Welcome from "./Welcome";
-import Carousel from "./Carousel";
-import ProjectBoard from "./ProjectBoard";
-export { Guide, Subheaders, Welcome, Carousel, ProjectBoard };
